ac85c4485b434a2ed611ebff1692f511128ef249
Kevin <kevingabrielmdn@gmail.com>
2023-09-03 14:21:10 -0600

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { GaleriaComponent } from './componentes/galeria/galeria.component';
+import { VistaComponent } from './componentes/vista/vista.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    GaleriaComponent,
+    VistaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
